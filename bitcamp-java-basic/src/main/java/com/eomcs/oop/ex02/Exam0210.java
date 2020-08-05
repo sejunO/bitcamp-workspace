fd7557dc19b1a8203fb94bb3cdc1271eb56e41ba
sejunO <frame8916@gmail.com>
2020-08-05 19:52:25 +0900

    t h i s...

--- bitcamp-java-basic/src/main/java/com/eomcs/oop/ex02/Exam0210.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex02/Exam0210.java
@@ -1,29 +1,23 @@
 package com.eomcs.oop.ex02;
 
-
-import com.eomcs.util.Calculator;
-
+import com.eomcs.util.Cal;
 
 public class Exam0210 {
 
   public static void main(String[] args) {
-    Calculator c1 = new Calculator();
-    Calculator c2 = new Calculator();
+    Cal c = new Cal();
 
-    c1.plus(2);
-    c1.plus(3);
-    c1.minus(1);
-    c1.multiple(7);
-    c2.plus(2);
-    c1.divide(3);
 
-    printResult(c1.result);
-    printResult(c2.result);
+    c.plus(5900);
+    c.나누기(2);
+    c.plus(1400);
+    c.plus(900);
+
+
+    printValue(c.result);
   }
-  static void printResult(int value) {
-    System.out.println("----------------");
-    System.out.println("결과 ====>   " + value);
-    System.out.println("----------------");
+  static void printValue(int value) {
+    System.out.println(value);
   }
 }
 
---
+++ bitcamp-java-basic/src/main/java/com/eomcs/util/Cal.java
@@ -0,0 +1,18 @@
+package com.eomcs.util;
+
+public class Cal {
+  public int result = 0;
+
+  public void plus(int result) {
+    this.result += result;
+  }
+  public void minus(int a) {
+    result -= a;
+  }
+  public void multi(int a) {
+    result *= a;
+  }
+  public void 나누기(int a) {
+    result /= a;
+  }
+}
