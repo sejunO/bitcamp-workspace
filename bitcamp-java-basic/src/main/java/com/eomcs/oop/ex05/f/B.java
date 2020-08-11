86dff45ba0e48551c192b6eb52ee7ad67d96bb4e
sejunO <frame8916@gmail.com>
2020-08-11 20:28:22 +0900

    Day22

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/oop/ex05/f/B.java
@@ -0,0 +1,18 @@
+package com.eomcs.oop.ex05.f;
+
+public class B extends A{
+  int v2;
+
+  B() {
+    System.out.println("B() 생성자!");
+  }
+  void m2() {
+    System.out.println("B.m2()");
+  }
+}
+
+
+
+
+
+
