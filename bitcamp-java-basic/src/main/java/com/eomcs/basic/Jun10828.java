bac4ea45ce8cbabcdb2a4d50809c1e17b9e1955a
sejunO <frame8916@gmail.com>
2020-09-05 21:22:58 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/Jun10828.java
@@ -0,0 +1,11 @@
+package com.eomcs.basic;
+
+public class Jun10828 {
+  public static void main(String[] args) {
+
+  }
+
+  public void push () {
+
+  }
+}
--- bitcamp-java-project/src/main/java/com/eomcs/util/LinkedList.java
+++ bitcamp-java-project/src/main/java/com/eomcs/util/LinkedList.java
@@ -26,7 +26,7 @@ public class LinkedList<E> extends AbstractList<E> {
   // 용도?
   // - Node 클래스는 목록에서 각 항목의 값을 보관하는 객체로 역할을 수행한다.
   // 스태틱 클래스?
-  // - 여러 개의 MyLinkedList 객체가 공유하는 클래스이므로
+  // - 여러 개의 MyLinkedList 객체가 공유하는 클래스이므로 
   // 스태틱으로 Node 클래스를 설계한다.
   static class Node<E> {
     E value;
