9959bb42a693e557eb4620a60aada5c1130e8e36
sejunO <frame8916@gmail.com>
2020-10-21 21:01:44 +0900

    .

--- bitcamp-java-project-server/src/main/java/com/eomcs/util/ThreadPool/Worker.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/util/ThreadPool/Worker.java
@@ -29,11 +29,17 @@ public class Worker extends Thread{
       }
 
       task.run();
+
+
       System.out.printf(" %s 작업완료 \n", this.getName());
+
       workers.add(this);
+
       System.out.printf(" %s 복귀 완료 \n", this.getName());
+
     }
   }
+
   public void execute(Runnable task) throws Exception {
     Worker t;
     if (workers.isEmpty()) {
